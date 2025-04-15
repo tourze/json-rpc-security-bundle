506ef298f587ab9e9a51c22d842e82b2005387a8
tourze-ci <you@example.com>
2025-04-15 10:45:36 +0000

    feat(security): 添加 MethodPermission 属性以声明方法所需的权限

---
+++ src/Attribute/MethodPermission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tourze\JsonRPCSecurityBundle\Attribute;
+
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+/**
+ * 声明这个方法所需要的权限
+ */
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
+class MethodPermission extends AutoconfigureTag
+{
+    public const JSONRPC_PERMISSION_TAG = 'json_rpc_http_server.method_permission';
+
+    public function __construct(string $permission, ?string $title = null)
+    {
+        $entity = null;
+        if (str_contains($permission, '::')) {
+            [$entity] = explode('::', $permission, 2);
+        }
+
+        parent::__construct(self::JSONRPC_PERMISSION_TAG, [
+            'permission' => $permission,
+            'entity' => $entity,
+            'title' => $title,
+        ]);
+    }
+}
